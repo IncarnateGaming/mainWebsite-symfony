cf806668ceebad67ec457cf50788e3477efeca6c
Philip Lawrence <philip.lawrence.1992@gmail.com>
2019-11-12 17:06:14 -0500

    Playing a little with twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,9 +23,8 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'My first page already: %s',
-            $slug
-        ));
+        return $this->render('article/show.html.twig',[
+            'title' => ucwords(str_replace('-',' ', $slug)),
+        ]);
     }
 }
