beaaa16929fa82c0a8bc3da3c21b44bac252a008
Philip Lawrence <philip.lawrence.1992@gmail.com>
2019-12-23 19:41:01 -0500

    setup template for feats and added <hr/> before legal sections

--- src/Controller/Content/FeatController.php
+++ src/Controller/Content/FeatController.php
@@ -4,6 +4,8 @@
 namespace App\Controller\Content;
 
 
+use App\Entity\ChapterIntro;
+use App\Entity\IncarnateFeat;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,17 +28,28 @@ class FeatController extends AbstractController
      * @Route("/content/feat/", name="inc_feats")
      */
     public function feats(){
+        $featRepository = $this->em->getRepository(IncarnateFeat::class);
+        $feats = $featRepository->findAll();
+        $chapterRepository = $this->em->getRepository(ChapterIntro::class);
+        $intro = $chapterRepository->buildParagraphsByCategory('Feat Intro');
         return $this->render('content/feats.html.twig',[
             'genericParts' => $this->genericParts,
+            'loop'=>$feats,
+            'path'=>'inc_feat',
+            'intro'=>$intro,
         ]);
     }
     /**
      * @Route("/content/feat/{slug}", name="inc_feat")
      */
     public function feat($slug){
+        $featRepository = $this->em->getRepository(IncarnateFeat::class);
+        $feat=$featRepository->findOneBy(['fid'=>$slug]);
+//        $feat = findInRepository($slug,$featRepository);
         return $this->render('content/feat.html.twig',[
             'genericParts' => $this->genericParts,
             'slug' => $slug,
+            'feat'=>$feat,
         ]);
     }
 }
