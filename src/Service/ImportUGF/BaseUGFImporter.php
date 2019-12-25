dd37c2bc23e43e7c15022e4b58d6e3525d223820
Philip Lawrence <philip.lawrence.1992@gmail.com>
2019-12-24 20:55:27 -0500

    changed the import types to be embedded inside of baseUGFImporter instead of as a stand alone script

--- src/Service/ImportUGF/BaseUGFImporter.php
+++ src/Service/ImportUGF/BaseUGFImporter.php
@@ -15,6 +15,36 @@ class BaseUGFImporter
         $this->ugfFilePath = '../lib/xml/Incarnate-System.xml';
         $this->ugf = simplexml_load_file($this->ugfFilePath);
         $this->functions = $functions;
-        $this->incImportType = $incImportType = require('ImportTypes.php');
+        $this->incImportType = array(
+            'background'=>'back',
+            'backgroundFeature'=>'backF',
+            'class'=>'class',
+            'classTrait'=>'classTr',
+            'classArchetype'=>'classAr',
+            'classArchetypeTrait'=>'classArTr',
+            'equipment'=>'eq',
+            'weapon'=>'eqWe',
+            'armor'=>'armor',
+            'feat'=>'feat',
+            'magicProperty'=>'magicP',
+            'npc'=>'npc',
+            'race'=>'race',
+            'raceTrait'=>'raceTr',
+            'raceSubrace'=>'raceSub',
+            'raceSubraceTrait'=>'raceSubTr',
+            'riddle'=>'riddle',
+            'rule'=>'rule',
+            'skill'=>'skill',
+            'spell'=>'spell',
+            'table'=>'table',
+            'template'=>'template',
+            'chapter'=>'chap',
+            'section1'=>'sec1',
+            'section2'=>'sec2',
+            'section3'=>'sec3',
+            'section4'=>'sec4',
+            'section5'=>'sec5',
+            'section6'=>'sec6',
+        );
     }
 }
