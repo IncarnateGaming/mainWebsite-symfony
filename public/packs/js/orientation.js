d69a8f9a615bfadcf0d09aaf5880b10c825182fd
Philip Lawrence <philip.lawrence.1992@gmail.com>
2019-12-30 00:17:55 -0500

    tweaked orientation value to better strip header

--- public/packs/js/orientation.js
+++ public/packs/js/orientation.js
@@ -10,9 +10,9 @@ class IncarnateOrientation{
         }
     }
     static orientHorizontal(){
+        incLogo.style.display='block';
         if(screen.orientation.angle === 90 || screen.orientation.angle === 270){
             htmlTop.style.fontSize='4vh';
-            incLogo.style.display='block';
         }
         [].forEach.call(IncarnateOrientation.getBanners(), banner=>{
             banner.classList.add('flex-row');
@@ -20,9 +20,9 @@ class IncarnateOrientation{
         });
     }
     static orientVertical(){
+        incLogo.style.display = 'none';
         if(screen.orientation.angle ===0 || screen.orientation.angle===180) {
             htmlTop.style.fontSize = '2vh';
-            incLogo.style.display = 'none';
         }
         [].forEach.call(IncarnateOrientation.getBanners(), banner=>{
             banner.classList.add('flex-column');
