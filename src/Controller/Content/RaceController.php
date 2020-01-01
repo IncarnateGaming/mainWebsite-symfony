5553995379feb25c92a1c14beba5529e84178e6f
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-01-01 10:19:08 -0500

    tweaked margin on top button to only be on right, added raceTrait and subraceTrait pages that can only be triggered by fid (due to duplication of names)

--- src/Controller/Content/RaceController.php
+++ src/Controller/Content/RaceController.php
@@ -7,6 +7,8 @@ namespace App\Controller\Content;
 use App\Entity\ChapterIntro;
 use App\Entity\IncarnateRace;
 use App\Entity\IncarnateRaceSubrace;
+use App\Entity\IncarnateRaceSubraceTrait;
+use App\Entity\IncarnateRaceTrait;
 use App\Repository\IncarnateRaceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -46,9 +48,9 @@ class RaceController extends AbstractController
      */
     public function race(string $slug, IncarnateRaceRepository $raceRepository){
 //        $raceRepository = $this->em->getRepository(IncarnateRace::class);
-        $race = $raceRepository->findOneBy(['fid'=>$slug]);
+//        $race = $raceRepository->findOneBy(['fid'=>$slug]);
 //        dump($race);die;
-//        $race = findInRepository($slug,$raceRepository);
+        $race = findInRepository($slug,$raceRepository);
         if($race){
             return $this->render('content/race.html.twig',[
                 'genericParts' => $this->genericParts,
@@ -57,12 +59,31 @@ class RaceController extends AbstractController
             ]);
         }
         $subraceRepository = $this->em->getRepository(IncarnateRaceSubrace::class);
-        $subrace = $subraceRepository->findOneBy(['fid'=>$slug]);
-        if($subrace){
-            return $this->render('content/racesubrace.html.twig',[
+//        $subrace = $subraceRepository->findOneBy(['fid'=>$slug]);
+        $subrace = findInRepository($slug,$subraceRepository);
+        if($subrace) {
+            return $this->render('content/racesubrace.html.twig', [
                 'genericParts' => $this->genericParts,
                 'slug' => $slug,
-                'subrace'=>$subrace,
+                'subrace' => $subrace,
+            ]);
+        }
+        $raceTraitRepository = $this->em->getRepository(IncarnateRaceTrait::class);
+        $trait = $raceTraitRepository->findOneBy(['fid'=>$slug]);
+        if($trait) {
+            return $this->render('content/racetrait.html.twig', [
+                'genericParts' => $this->genericParts,
+                'slug' => $slug,
+                'trait' => $trait,
+            ]);
+        }
+        $subraceTraitRepository = $this->em->getRepository(IncarnateRaceSubraceTrait::class);
+        $subraceTrait = $subraceTraitRepository->findOneBy(['fid'=>$slug]);
+        if($subraceTrait){
+            return $this->render('content/racesubracetrait.html.twig',[
+                'genericParts' => $this->genericParts,
+                'slug' => $slug,
+                'trait' => $subraceTrait,
             ]);
         }else{
             throw $this->createNotFoundException('The race: "' . $slug . '" does not exist');
