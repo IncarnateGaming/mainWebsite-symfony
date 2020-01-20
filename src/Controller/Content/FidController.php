c2893370fddd4de4300fb9f5e81a19e67e9b2179
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-01-20 12:45:22 -0500

    made /fid recognize category fids

--- src/Controller/Content/FidController.php
+++ src/Controller/Content/FidController.php
@@ -4,6 +4,7 @@
 namespace App\Controller\Content;
 
 
+use App\Entity\ChapterIntro;
 use App\Entity\IncarnateItem;
 use App\Service\ImportUGF\BaseUGFImporter;
 use Doctrine\ORM\EntityManagerInterface;
@@ -29,44 +30,51 @@ class FidController extends AbstractController
         $incImportType = $baseUGFImporter->incImportType;
         $repository = $this->em->getRepository(IncarnateItem::class);
         $item = $repository->findOneBy(['fid'=>$slug]);
-        if(!$item){
-            throw $this->createNotFoundException('The fid: "' . $slug . '" does not exist');
-        }
-        $type = $item->getType();
-        if ($incImportType['background']==$type||['backgroundFeature']==$type){
-            return $this->redirect($this->generateUrl('inc_background',['slug'=>$slug]));
-        }elseif ($incImportType['class']==$type||$incImportType['classTrait']==$type||$incImportType['classArchetype']==$type||$incImportType['classArchetypeTrait']==$type){
-            return $this->redirect($this->generateUrl('inc_class',['slug'=>$slug]));
-        }elseif ($incImportType['equipment']==$type||$incImportType['weapon']==$type||$incImportType['armor']==$type){
-            return $this->redirect($this->generateUrl('inc_equipment',['slug'=>$slug]));
-        }elseif ($incImportType['feat']==$type) {
-            return $this->redirect($this->generateUrl('inc_feat',['slug'=>$slug]));
-        }elseif($incImportType['magicProperty']==$type){
-            return $this->redirect($this->generateUrl('inc_magic_property',['slug'=>$slug]));
-        }elseif($incImportType['npc']==$type){
-            return $this->redirect($this->generateUrl('inc_npc',['slug'=>$slug]));
-        }elseif($incImportType['race']==$type||$incImportType['raceTrait']==$type||$incImportType['raceSubrace']==$type||$incImportType['raceSubraceTrait']==$type){
-            return $this->redirect($this->generateUrl('inc_race',['slug'=>$slug]));
-        }elseif($incImportType['raceSubrace']==$type||$incImportType['raceTrait']==$type||$incImportType['raceSubrace']==$type||$incImportType['raceSubraceTrait']==$type){
-            return $this->redirect($this->generateUrl('inc_race',['slug'=>$slug]));
-        }elseif($incImportType['raceTrait']==$type||$incImportType['raceTrait']==$type||$incImportType['raceSubrace']==$type||$incImportType['raceSubraceTrait']==$type){
-            return $this->redirect($this->generateUrl('inc_race',['slug'=>$slug]));
-        }elseif($incImportType['raceSubraceTrait']==$type||$incImportType['raceTrait']==$type||$incImportType['raceSubrace']==$type||$incImportType['raceSubraceTrait']==$type){
-            return $this->redirect($this->generateUrl('inc_race',['slug'=>$slug]));
-        }elseif($incImportType['riddle']==$type){
-            return $this->redirect($this->generateUrl('inc_riddle',['slug'=>$slug]));
-        }elseif($incImportType['rule']==$type){
-            return $this->redirect($this->generateUrl('inc_rule',['slug'=>$slug]));
-        }elseif($incImportType['skill']==$type){
-            return $this->redirect($this->generateUrl('inc_skill',['slug'=>$slug]));
-        }elseif($incImportType['spell']==$type){
-            return $this->redirect($this->generateUrl('inc_spell',['slug'=>$slug]));
-        }elseif ($incImportType['table']==$type){
-            return $this->redirect($this->generateUrl('inc_table',['slug'=>$slug]));
-        }elseif($incImportType['template']==$type){
-            return $this->redirect($this->generateUrl('inc_template',['slug'=>$slug]));
-        }else{
-            throw $this->createNotFoundException('The fid: "' . $slug . '" does not have a valid type.');
+        if($item) {
+            $type = $item->getType();
+            if ($incImportType['background'] == $type || ['backgroundFeature'] == $type) {
+                return $this->redirect($this->generateUrl('inc_background', ['slug' => $slug]));
+            } elseif ($incImportType['class'] == $type || $incImportType['classTrait'] == $type || $incImportType['classArchetype'] == $type || $incImportType['classArchetypeTrait'] == $type) {
+                return $this->redirect($this->generateUrl('inc_class', ['slug' => $slug]));
+            } elseif ($incImportType['equipment'] == $type || $incImportType['weapon'] == $type || $incImportType['armor'] == $type) {
+                return $this->redirect($this->generateUrl('inc_equipment', ['slug' => $slug]));
+            } elseif ($incImportType['feat'] == $type) {
+                return $this->redirect($this->generateUrl('inc_feat', ['slug' => $slug]));
+            } elseif ($incImportType['magicProperty'] == $type) {
+                return $this->redirect($this->generateUrl('inc_magic_property', ['slug' => $slug]));
+            } elseif ($incImportType['npc'] == $type) {
+                return $this->redirect($this->generateUrl('inc_npc', ['slug' => $slug]));
+            } elseif ($incImportType['race'] == $type || $incImportType['raceTrait'] == $type || $incImportType['raceSubrace'] == $type || $incImportType['raceSubraceTrait'] == $type) {
+                return $this->redirect($this->generateUrl('inc_race', ['slug' => $slug]));
+            } elseif ($incImportType['raceSubrace'] == $type || $incImportType['raceTrait'] == $type || $incImportType['raceSubrace'] == $type || $incImportType['raceSubraceTrait'] == $type) {
+                return $this->redirect($this->generateUrl('inc_race', ['slug' => $slug]));
+            } elseif ($incImportType['raceTrait'] == $type || $incImportType['raceTrait'] == $type || $incImportType['raceSubrace'] == $type || $incImportType['raceSubraceTrait'] == $type) {
+                return $this->redirect($this->generateUrl('inc_race', ['slug' => $slug]));
+            } elseif ($incImportType['raceSubraceTrait'] == $type || $incImportType['raceTrait'] == $type || $incImportType['raceSubrace'] == $type || $incImportType['raceSubraceTrait'] == $type) {
+                return $this->redirect($this->generateUrl('inc_race', ['slug' => $slug]));
+            } elseif ($incImportType['riddle'] == $type) {
+                return $this->redirect($this->generateUrl('inc_riddle', ['slug' => $slug]));
+            } elseif ($incImportType['rule'] == $type) {
+                return $this->redirect($this->generateUrl('inc_rule', ['slug' => $slug]));
+            } elseif ($incImportType['skill'] == $type) {
+                return $this->redirect($this->generateUrl('inc_skill', ['slug' => $slug]));
+            } elseif ($incImportType['spell'] == $type) {
+                return $this->redirect($this->generateUrl('inc_spell', ['slug' => $slug]));
+            } elseif ($incImportType['table'] == $type) {
+                return $this->redirect($this->generateUrl('inc_table', ['slug' => $slug]));
+            } elseif ($incImportType['template'] == $type) {
+                return $this->redirect($this->generateUrl('inc_template', ['slug' => $slug]));
+            } else {
+                throw $this->createNotFoundException('The fid: "' . $slug . '" does not have a valid type.');
+            }
+        }else {
+            $ruleRepository = $this->em->getRepository(ChapterIntro::class);
+            $item = $ruleRepository->findOneBy(['categoryFid'=>$slug]);
+            if($item) {
+                return $this->redirect($this->generateUrl('inc_rule',['slug'=>$slug]));
+            }else {
+                throw $this->createNotFoundException('The fid: "' . $slug . '" does not exist');
+            }
         }
     }
 }
